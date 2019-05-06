05fe0196dd147aec1730afce9543e0754894bb0f
ArtemNizelnyk <artem.nizelnik@gmail.com>
2019-05-06 16:05:40 +0800

    Fix-Commit

---
+++ src/Sitecore.Support.328606/EDS/Providers/SparkPost/Dispatch/DispatchProvider.cs
@@ -0,0 +1,23 @@
+﻿namespace Sitecore.Support.EDS.Providers.SparkPost.Dispatch
+{
+  using Sitecore.EDS.Core.Dispatch;
+  using Sitecore.EDS.Core.Reporting;
+  using Sitecore.EDS.Providers.SparkPost.Configuration;
+  using Sitecore.EDS.Providers.SparkPost.Dispatch;
+  using Sitecore.StringExtensions;
+  public class DispatchProvider : Sitecore.EDS.Providers.SparkPost.Dispatch.DispatchProvider
+  {
+    public DispatchProvider(ConnectionPoolManager connectionPoolManager, IEnvironmentId environmentIdentifier, IConfigurationStore configurationStore, string returnPath) : base(connectionPoolManager, environmentIdentifier, configurationStore, returnPath)
+    {
+    }
+    protected override void SetMessageHeaders(EmailMessage message)
+    {
+      base.SetMessageHeaders(message);
+
+      if (!message.FromAddress.IsNullOrEmpty())
+      {
+        message.Headers.Set("Sender", message.FromAddress);
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.328606/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.328606")]
+[assembly: AssemblyProduct("Sitecore.Support.328606")]
+[assembly: ComVisible(false)]

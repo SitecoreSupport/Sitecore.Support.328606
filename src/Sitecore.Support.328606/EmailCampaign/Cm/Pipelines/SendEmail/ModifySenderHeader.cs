27baaf95a7bdbf5a52a32e256ecc4f12abaec749
EugeneOparyshev <eop@sitecore.net>
2019-11-04 14:14:24 +0200

    github patch

---
+++ src/Sitecore.Support.328606/EmailCampaign/Cm/Pipelines/SendEmail/ModifySenderHeader.cs
@@ -0,0 +1,22 @@
+﻿namespace Sitecore.Support.EmailCampaign.Cm.Pipelines.SendEmail
+{
+    using Sitecore.Diagnostics;
+    using Sitecore.EDS.Core.Dispatch;
+    using Sitecore.EmailCampaign.Cm.Pipelines.SendEmail;
+    public class ModifySenderHeader
+    {
+        public void Process(SendMessageArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            EmailMessage emailMessage = args.CustomData["EmailMessage"] as EmailMessage;
+            if (emailMessage.FromName.Length > 0)
+            {
+                emailMessage.Headers.Set("Sender", emailMessage.FromName + "<" + emailMessage.FromAddress + " > ");
+            }
+            else
+            {
+                emailMessage.Headers.Remove("Sender");
+            }
+        }
+    }
+}
